cbccd9834dc4ebaa99bd415caa37916d24812cef
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-09 12:26:11 -0400

    eslint fixer

--- ComplexAPI/index.js
+++ ComplexAPI/index.js
@@ -1,3 +1,4 @@
+/* global $ */
 'use strict';
 
 // API Key - 3YSXsi21xtIA39YGNOR2Oha3R82mQ0Th7LYa0Wzc
@@ -15,67 +16,67 @@ const apiKey = '3YSXsi21xtIA39YGNOR2Oha3R82mQ0Th7LYa0Wzc';
 const searchURL = `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}`;
 
 const store = {
-    parks: [],
-    errorMessage: null,
-    formReady: false,
+  parks: [],
+  errorMessage: null,
+  formReady: false,
 };
 
 function displayResults(responseJson) {
-    console.log(responseJson);
-    $('.results-list').empty();
-    for (let i = 0; i < responseJson.data.length; i++) {
-        $('.results-list').append(
-            `<li>
+  console.log(responseJson);
+  $('.results-list').empty();
+  for (let i = 0; i < responseJson.data.length; i++) {
+    $('.results-list').append(
+      `<li>
                 <h3><a href= "${responseJson.data[i].url}">${responseJson.data[i].name}</a></h3>
                 <p>${responseJson.data[i].description}</p>
             </li>`
-        );
-    }
+    );
+  }
 }
 $('.results').removeClass('hidden');
 
 
 function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
-    return queryItems.join('&');
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+  return queryItems.join('&');
 }
 
 function requestPark(query, maxNumber) {
-    console.log('Request park with query and limit it to maxNum');
-    const params = {
-        stateCode: query, 
-        limit: maxNumber, 
-        api_key: apiKey,
-    };
-    const queryString = formatQueryParams(params);
-    const url = searchURL + '&' + queryString;
+  console.log('Request park with query and limit it to maxNum');
+  const params = {
+    stateCode: query, 
+    limit: maxNumber, 
+    api_key: apiKey,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchURL + '&' + queryString;
 
-    console.log(url);
+  console.log(url);
 
-    fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then(responseJson => displayResults(responseJson, maxNumber))
-        .catch(e => {
-            $('.error-message').text(`Something went wrong: ${e.message}`);
-        });
-}
-
-function watchForm() {
-    $('#park-search').on('submit', event => {
-        event.preventDefault();
-        const searchTerm = event.target.querySearch.value;
-        const maxResults = event.target.maxNum.value;
-        requestPark(searchTerm, maxResults);
-        console.log(`${searchTerm} ${maxResults}`);
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson, maxNumber))
+    .catch(e => {
+      $('.error-message').text(`Something went wrong: ${e.message}`);
     });
 }
 
+function watchForm() {
+  $('#park-search').on('submit', event => {
+    event.preventDefault();
+    const searchTerm = event.target.querySearch.value;
+    const maxResults = event.target.maxNum.value;
+    requestPark(searchTerm, maxResults);
+    console.log(`${searchTerm} ${maxResults}`);
+  });
+}
+
 $(function () {
-    watchForm();
+  watchForm();
 });
